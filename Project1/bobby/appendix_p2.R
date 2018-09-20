acb513e2ba41e341769c420d77522857441078be
Bobby Butler <bbutler3@iu.edu>
2018-09-19 23:55:29 -0400

    more power

--- Project1/bobby/appendix_p2.R
+++ Project1/bobby/appendix_p2.R
@@ -1,13 +1,30 @@
-nlist = seq(10,1000,10)
+library(lsr)
+library(pwr)
+
+#nlist = seq(10,1000,10)
+nlist = 10
 
 set.seed(333)
 
 for (n in nlist){
-  sub1 = matrix(rnorm(100*n,1,1),n,100)
-  sub2 = matrix(rnorm(100*n,1.2,1),n,100)
+  sub1 = as.data.frame(matrix(rnorm(100*n,1,1),n,100))
+  sub2 = as.data.frame(matrix(rnorm(100*n,1.2,1),n,100))
   
-  comparisons <- mapply(function(sub1,sub2){t.test(as.data.frame(sub1),
-                                                   as.data.frame(sub2),
-                                                   alternative = 'two.sided',
-                                                   var.equal = TRUE)})
+  #means1 = colMeans(sub1)
+  #means2 = colMeans(sub2)
+  
+  comparisons <- mapply(function(s1,s2){t.test(s1,s2,alternative = 'two.sided',
+                                           var.equal = TRUE)},sub1,sub2)
+  
+  cohD <- mapply(function(s1,s2){cohensD(s1,s2)},sub1,sub2)
+  
+  comparisons = as.data.frame(comparisons)
+  
+  compsnew <- sapply(comparisons, function(x){c(x$statistic,
+                                                p_value = x$p.value,
+                                                m1 = x$estimate[1],
+                                                m2 = x$estimate[2])})
+
+  compsnew <- as.data.frame(t(compsnew))
+  names(compsnew) <- c("t","p","m1","m2")
 } 
--- Project1/bobby/means_tar.R
+++ Project1/bobby/means_tar.R
@@ -16,13 +16,12 @@ for (l in letters){
   compsnew <- sapply(comparisons, function(x){c(round(x$statistic,4),
                                                p_value = round(x$p.value,4),
                                                conf_low = round(x$conf.int[1],4),
-                                               conf_high = round(x$conf.int[2],4))
-  })
+                                               conf_high = round(x$conf.int[2],4))})
   
   compsnew <- t(compsnew)
   
-  writefile = paste(l,'means.csv',sep='')
-  write.csv(compsnew,writefile,quote = FALSE,row.names = FALSE)
+  #writefile = paste(l,'means.csv',sep='')
+  #write.csv(compsnew,writefile,quote = FALSE,row.names = FALSE)
   
   significant_diffs = sum(as.double(compsnew[101:200])<0.05)
   
