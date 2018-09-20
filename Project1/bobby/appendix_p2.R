e1c3302f6563f55960ac6bfe846d924ac8adb0c9
Bobby Butler <bbutler3@iu.edu>
2018-09-20 00:35:15 -0400

    i made plots

--- Project1/bobby/appendix_p2.R
+++ Project1/bobby/appendix_p2.R
@@ -1,17 +1,19 @@
 library(lsr)
 library(pwr)
 
-#nlist = seq(10,1000,10)
-nlist = 10
-
 set.seed(333)
+
+effsize = 0.2
+alpha = 0.05
+
+nlist = seq(10,1000,10)
+
+means = list()
+powers = list()
 
 for (n in nlist){
   sub1 = as.data.frame(matrix(rnorm(100*n,1,1),n,100))
   sub2 = as.data.frame(matrix(rnorm(100*n,1.2,1),n,100))
-  
-  #means1 = colMeans(sub1)
-  #means2 = colMeans(sub2)
   
   comparisons <- mapply(function(s1,s2){t.test(s1,s2,alternative = 'two.sided',
                                            var.equal = TRUE)},sub1,sub2)
@@ -27,4 +29,34 @@ for (n in nlist){
 
   compsnew <- as.data.frame(t(compsnew))
   names(compsnew) <- c("t","p","m1","m2")
+  
+  sigdiffs <- compsnew[compsnew$p < alpha,]
+  meansig <- mean(sigdiffs$m1 - sigdiffs$m2)
+  means <- append(means,meansig)
+  
+  powertest = pwr.t.test(n=n,d=effsize,sig.level=alpha,type='two.sample',alternative='two.sided')
+  powers <- append(powers,powertest$power)
 } 
+
+df <- as.data.frame(cbind(nlist,means,powers))
+
+### means plot
+pdf('plots/means_vs_n.pdf',width=8,height=5)
+par(las=1)
+par(cex.lab=1.2,cex.main=1.4,family="Helvetica",mar=c(4,4,3,2)+0.1)
+plot(df$nlist,df$means,main='',xlab='',ylab='',col='blue',
+     panel.first=abline(h=-0.2,col="red"))
+title(main='Statistically Significant Mean Differences vs. n',line=1.2)
+title(ylab='Mean of Significant Mean Differences',line=3)
+title(xlab='n',line=2.4)
+dev.off()
+
+### powers plot
+pdf('plots/means_vs_powers.pdf',width=8,height=5)
+par(las=1)
+par(cex.lab=1.2,cex.main=1.4,family="Helvetica",mar=c(4,4,3,2)+0.1)
+plot(df$powers,df$means,main='',xlab='',ylab='',col='blue')
+title(main='Statistically Significant Mean Differences vs. Power',line=1.2)
+title(ylab='Mean of Significant Mean Differences',line=3)
+title(xlab='Power',line=2.4)
+dev.off()
