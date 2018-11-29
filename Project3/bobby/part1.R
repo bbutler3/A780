139e5eefd444ff99c1d50018c1dc191c7b4fcbe6
Bobby Butler <bbutler3ky@gmail.com>
2018-11-29 12:55:01 -0500

    more coding

--- Project3/bobby/part1.R
+++ Project3/bobby/part1.R
@@ -12,6 +12,7 @@ nbins = 20
 trials = 200
 
 pl <- rpower(N,origalpha)
+pl2 <- unlist(read.table("../powerlaw.dat"))
 
 llsq_hist <- function(pl,n,nbins,trials){
   alist = {}
@@ -40,15 +41,51 @@ llsq_hist <- function(pl,n,nbins,trials){
     numobs = append(numobs,num)
   }
   alphamean = mean(alist)
+  alphamed = median(alist)+1
+  stdev = sd(alist)
   finalpha = alphamean+1
   finerr = sqrt(sum(aerrlist^2))/trials
   finerr = (finerr/alphamean)*finalpha
   
-  return(list("n"=n,"bins"=nbins,"trials"=trials,"alpha"=finalpha,"error"=finerr))
+  return(list("allalpha"=alist,"n"=n,"bins"=nbins,"trials"=trials,"alpha"=finalpha,"medalpha"=alphamed,"error"=finerr,"stdev"=stdev))
 }
 
-llsq_weight <- function(){
-  x=1
+llsq_weight <- function(pl,n,nbins,trials){
+  alist = {}
+  aerrlist = {}
+  numobs = {}
+  for (i in seq(trials)){
+    sdata = sample(pl,n)
+    sdatalog = log10(sdata)
+    
+    bins = binning(sdatalog,nclass=nbins)
+    breaks = bins$breaks
+    cts = bins$counts
+    adds = diff(breaks)*0.5
+    xvals = head(breaks,-1) + adds
+    yvals = log10(cts)
+    yvals[which(yvals==-Inf)] = NA
+    
+    weights = (yvals*n)/((n-yvals)*(log10(exp(1)))^2)
+    
+    logdata = data.frame(xvals,yvals)
+    fit = lm(yvals ~ xvals,logdata,weights=weights,na.action=na.exclude)
+    out = summary(fit)
+    alpha = out$coefficients[2,1]
+    alphaerr = out$coefficients[2,2]
+    alist = append(alist,-alpha)
+    aerrlist = append(aerrlist,alphaerr)
+    num = length(fit$residuals)
+    numobs = append(numobs,num)
+  }
+  alphamean = mean(alist)
+  alphamed = median(alist)+1
+  stdev = sd(alist)
+  finalpha = alphamean+1
+  finerr = sqrt(sum(aerrlist^2))/trials
+  finerr = (finerr/alphamean)*finalpha
+  
+  return(list("allalpha"=alist,"n"=n,"bins"=nbins,"trials"=trials,"alpha"=finalpha,"medalpha"=alphamed,"error"=finerr,"stdev"=stdev))
 }
 
 llsq_ecdf <- function(pl,n,trials){
@@ -73,11 +110,13 @@ llsq_ecdf <- function(pl,n,trials){
     numobs = append(numobs,num)
   }
   alphamean = mean(alist)
+  alphamed = median(alist)+1
+  stdev = sd(alist)
   finalpha = alphamean+1
   finerr = sqrt(sum(aerrlist^2))/trials
   finerr = (finerr/alphamean)*finalpha
   
-  return(list("n"=n,"trials"=trials,"alpha"=finalpha,"error"=finerr))
+  return(list("allalpha"=alist,"n"=n,"trials"=trials,"alpha"=finalpha,"medalpha"=alphamed,"error"=finerr,"stdev"=stdev))
 }
   
 mle_est <- function(pl,n,trials){
@@ -94,15 +133,30 @@ mle_est <- function(pl,n,trials){
     aerrlist = append(aerrlist,alphaerr)
   }
   alphamean = mean(alist)
+  alphamed = median(alist)
+  stdev = sd(alist)
   finalpha = alphamean
   finerr = sqrt(sum(aerrlist^2))/trials
   finerr = (finerr/alphamean)*finalpha
   
-  return(list("n"=n,"trials"=trials,"alpha"=finalpha,"error"=finerr))
+  return(list("allalpha"=alist,"n"=n,"trials"=trials,"alpha"=finalpha,"medalpha"=alphamed,"error"=finerr,"stdev"=stdev))
 }
 
-moments <- function(){
-  x=1
+moments <- function(pl,n,trials){
+  alist = {}
+  aerrlist = {}
+  numobs = {}
+  for (i in seq(trials)){
+    sdata = sample(pl,n)
+    mu = mean(sdata)
+    alpha = (2*mu-1)/(mu-1)
+    alist = append(alist,alpha)
+  }
+  alphamean = mean(alist)
+  alphamed = median(alist)
+  stdev = sd(alist)
+  
+  return(list("allalpha"=alist,"n"=n,"trials"=trials,"alpha"=alphamean,"medalpha"=alphamed,"stdev"=stdev))
 }
 
 nllsq <- function(pl,n,trials){
@@ -110,43 +164,24 @@ nllsq <- function(pl,n,trials){
   aerrlist = {}
   numobs = {}
   for (i in seq(trials)){
-    
+    sdata = sample(pl,n)
+    tabdata = table(sdata)
+    df = as.data.frame(tabdata)
+    x = as.numeric(unlist(df[1]))
+    y = as.numeric(unlist(df[2]))
+    fit = nls(y ~ a*x^(-b-1))
+    print(fit)
   }
 }
 
-reshist = llsq_hist(pl,n,nbins,trials)
-resecdf = llsq_ecdf(pl,n,trials)
-resmle = mle_est(pl,n,trials)
+#reshist = llsq_hist(pl,n,nbins,trials)
+#resweight = llsq_weight(pl,n,nbins,trials)
+#resecdf = llsq_ecdf(pl,n,trials)
+#resmle = mle_est(pl,n,trials)
+#resmoments = moments(pl,n,trials)
+#resnllsq = nllsq(pl,n,1)
 
-# for (i in seq(trials)){
-#   sdata = sample(pl,n)
-#   sdatalog = log10(sdata)
-#   
-#   bins = binning(sdatalog,nclass=nbins)
-#   breaks = bins$breaks
-#   cts = bins$counts
-#   adds = diff(breaks)*0.5
-#   xvals = head(breaks,-1) + adds
-#   yvals = log10(cts)
-#   yvals[which(yvals==-Inf)] = NA
-#   
-#   logdata = data.frame(xvals,yvals)
-#   fit = lm(yvals ~ xvals,logdata,na.action=na.exclude)
-#   out = summary(fit)
-#   alpha = out$coefficients[2,1]
-#   alphaerr = out$coefficients[2,2]
-#   alist = append(alist,-alpha)
-#   aerrlist = append(aerrlist,alphaerr)
-#   num = length(fit$residuals)
-#   numobs = append(numobs,num)
-# }
-# 
-# alphamean = mean(alist)
-# finalpha = alphamean+1
-# print(finalpha)
-# finalerr = sqrt(sum(aerrlist^2))/trials
-# finalerr = (finalerr/alphamean)*finalpha
-# print(finalerr)
+print(nllsq(pl2,n,trials))
 
-sortuniq <- sort(unique(pl))
-empir = ecdf(pl)
+#final = list(c(reshist$alpha,reshist$error),c(resweight$alpha,resweight$error),c(resecdf$alpha,resecdf$error),
+#             c(resmle$alpha,resmle$error),resmoments$alpha,c(resnllsq$alpha,resnllsq$error))
